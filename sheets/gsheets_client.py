adbc70ccb7c088d2bd73dbac50c3b3bb463c4948
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2021-10-07 00:22:48 -0700

    Creating class wrapper for Google Sheets API

---
+++ sheets/gsheets_client.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+class Sheets():
+
+    def __init__(self):
+    # If modifying these scopes, delete the file token.json.
+        SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(os.path.join(os.path.relpath(os.getcwd()), 'sheets/credentials.json'), SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        
+        service = build('sheets', 'v4', credentials=creds)
