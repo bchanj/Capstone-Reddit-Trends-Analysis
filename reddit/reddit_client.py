e04adf9770b4fd80b0ad1835e48492ec336b9007
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2021-10-07 04:16:55 +0000

    Patch Internal API Wrapper Code to include only necessary changes

--- reddit/reddit_client.py
+++ reddit/reddit_client.py
@@ -12,21 +12,4 @@ class reddit_client:
             user_agent="testscript",
             username="North-Alternative886",
         )
-        self.reddit.read_only = True
-
-    def get_sub_reddit(self, sub_reddit_name: str):
-        return self.reddit.subreddit(sub_reddit_name)
-
-    def get_sub_reddit_submissions(self, sub_reddit_name: str):
-        subreddit = self.get_sub_reddit(sub_reddit_name)
-        # assume you have a Subreddit instance bound to variable `subreddit`
-        for submission in subreddit.hot(limit=10):
-            print(submission.title)
-            # Output: the submission's title
-            print(submission.score)
-            # Output: the submission's score
-            print(submission.id)
-            # Output: the submission's ID
-            print(submission.url)
-            # Output: the URL the submission points to or the submission's URL if it's a self post
-    
+        self.reddit.read_only = True # change if editing
