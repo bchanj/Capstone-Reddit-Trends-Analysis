7f91fe1dfe03374aaefb192209c098b9381a3b38
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2021-10-07 22:39:32 -0700

    Revising gsheets_client to use gsheets wrapper, not Google Sheets API directly.

--- sheets/gsheets_client.py
+++ sheets/gsheets_client.py
@@ -1,41 +1,13 @@
-from __future__ import print_function
-import os
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+import gspread
+from google.oauth2.service_account import Credentials
+from typing import List
+
 
 class Sheets():
-
     def __init__(self):
-    # If modifying these scopes, delete the file token.json.
-        SCOPES = ['https://spreadsheets.google.com/feeds',
-            "https://www.googleapis.com/auth/spreadsheets",
-            'https://www.googleapis.com/auth/drive']
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(os.path.join(os.path.relpath(os.getcwd()), 'sheets/credentials.json'), SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-        
-        self.service = build('sheets', 'v4', credentials=creds)
-
-    def create(self, title: str) -> None:
-        spreadsheet = {
-            'properties': {
-                'title': title
-            }
-        }
-        spreadsheet = self.service.spreadsheets().create(body=spreadsheet,
-                                            fields='spreadsheetId').execute()
+        # If modifying these scopes, delete the file token.json.
+        self._scope: List[str] = ['https://spreadsheets.google.com/feeds',
+                                  'https://www.googleapis.com/auth/drive']
+        creds = Credentials.from_service_account_file(
+            'service_credentials.json', scopes=self._scope)
+        self.gspread_client = gspread.authorize(creds)
