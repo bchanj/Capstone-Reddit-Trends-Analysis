bd9085aa7156c3d583d9cad4821e5c18211525ef
onalanbaran <onalanbaran@seattleu.edu>
2021-10-13 23:47:51 -0700

    Pushing heuristic

--- reddit/reddit_client.py
+++ reddit/reddit_client.py
@@ -1,5 +1,5 @@
 import praw
-
+import re
 
 class reddit_client:
     """
@@ -21,3 +21,29 @@ class reddit_client:
         for submission in gd.hot(limit=5):
             print(submission.title)
 
+
+    def getDeal():
+        for submission in reddit.subreddit("GameDeals").new(limit=100):
+          title = submission.title
+          print('Title : ' + title)
+          # for each title, extract certain data using Regular Expressions
+          # The template is as follows: [Store Name] Deal Information (Price/percent off)
+          # A full example might be: [Humble Store] Psychonauts ($1.99/80% off)
+          m = re.match(r"^\[(.+?)\] (.+?) \((.+?)\)", title)
+          if m is None:
+            print("!!! TITLE DIDNOT MATCH THE EXPECTED FORMAT !!!")
+          else:
+            merchant = m[1]
+            print('Merchant: ' + merchant)
+            deal = m[2]
+            print('Deal: ' + deal)
+            priceNpercent = m[3]
+            m_price = re.search(r"\$\d+(\.\d+)?", priceNpercent)
+            if m_price is not None:
+              print('Price: ' + m_price[0])
+            m_percent = re.search(r"\d+(\.\d+)?%", priceNpercent)
+            if m_percent is not None:
+              print('Percent off: ' + m_percent[0])
+            url = submission.url
+            print('url: ' + url)
+          print()
