ffccf834d895d81a303800ff4a5c3bd67fa67150
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2021-11-13 17:37:21 -0800

    Newest Version

--- reddit/MUAontheCheap_subreddit.py
+++ reddit/MUAontheCheap_subreddit.py
@@ -7,6 +7,7 @@ class MUAontheCheapDeal(reddit_client):
         super(MUAontheCheapDeal,self).__init__()
         self.makeupCompanies = {} #Dictonary used to store <company name, list of posts>
         self.redditPosts = [] #Contains all posts read in by file.
+        
 
     #Pre-Condition : Valid MakeupCompanies.txt and MUAhot.txt
     #Post-Condition : None
@@ -111,9 +112,12 @@ class MUAontheCheapDeal(reddit_client):
         return potentialName
 
     def ValidateCompany(self, results): #MAKE SURE U USE TYPES 
-        fail = open("failPosts.txt", "a")
+
+        #Create an array with common words.
+        #
         postTitle = ""
         companyName = ""
+        potenName = ""
         successPosts = [()]
         failPosts = [()]
         i = 0
@@ -122,28 +126,50 @@ class MUAontheCheapDeal(reddit_client):
                 continue
             postTitle = userSubmission.title
             print("Title: " + postTitle)
+       
             #Post Body
-            potenName = self.FindDomain(userSubmission.selftext.replace("\n", ""))
-            if potenName.lower() in postTitle.lower():
-                companyName = potenName
+            companyUrl = self.FindDomain(userSubmission.selftext.replace("\n", ""))
+            if companyUrl != "":
+                for word in postTitle.split():
+                    if word[0].isupper() == True:
+                        word = ''.join(c for c in word if c.isalpha()) #Make sure only letters are compared
+                        if word.lower() in companyUrl:
+                            companyUrl = companyUrl.replace(word.lower(), "")
+                            companyName += word + " "
+                if companyUrl != "":
+                    companyName = ""
             print("Body: " + self.FindDomain(userSubmission.selftext.replace("\n", "")))
+            
             #Post Url
-            potenName = self.FindDomain(userSubmission.url.replace("\n", ""))
-            if companyName == "" and potenName.lower() in postTitle.lower():
-                    companyName = potenName
+            if companyName == "":
+                companyUrl = self.FindDomain(userSubmission.url.replace("\n", ""))
+                for word in postTitle.split():
+                    if word[0].isupper() == True:
+                        word = ''.join(c for c in word if c.isalpha()) #Make sure only letters are compared
+                        if word.lower() in companyUrl:
+                            companyUrl = companyUrl.replace(word.lower(), "")
+                            companyName += word + " "
+                if companyUrl != "":
+                    companyName = ""
             print("Url: " + self.FindDomain(userSubmission.url.replace("\n", "")))
+            
             #Post Comments
             if companyName == "":
                 for comment in userSubmission.comments:
                     if comment.is_submitter == True:
-                        potenName = self.FindDomain(comment.body.replace("\n", ""))
+                        companyUrl = self.FindDomain(comment.body.replace("\n", ""))
                         print("OP Comment: " + comment.body.replace("\n", ""))
-                        if potenName.lower() in postTitle.lower():
-                            companyName = potenName
+                        for word in postTitle.split():
+                            if word[0].isupper() == True:
+                                word = ''.join(c for c in word if c.isalpha()) #Make sure only letters are compared
+                                if word.lower() in companyUrl:
+                                    companyUrl = companyUrl.replace(word.lower(), "")
+                                    companyName += word + " "
+                        #if companyUrl != "":
+                        #    companyName = ""
                         #print("OP Comment: " + self.FindDomain(userSubmission.url.replace("\n", "")))
             
             print()
-            #If company  name still not found. 
             i = i + 1
             print(str(i) + " Title: " + postTitle)
             print("Company Name: " + companyName)
@@ -151,12 +177,13 @@ class MUAontheCheapDeal(reddit_client):
             if companyName != "":
                 successPosts.append((companyName, postTitle))
             else:
-                failPosts.append((userSubmission, postTitle))
-                fail.write(userSubmission.id + "\n")
-                fail.write(postTitle + "\n")
+                #Where we would put the dictonary to count freq.
+                failPosts.append(([companyUrl, companyName], postTitle))
             companyName = ""
-    
-        fail.close()
+            companyUrl = ""
+
+        #Parse through failPosts. Compare companyUrl to array of skip words.
+        #Add to successPosts if companyUrl is now empty.
 
         print("Correct Results: ")
         for captured in successPosts:
@@ -164,61 +191,10 @@ class MUAontheCheapDeal(reddit_client):
 
         print("Correct:" + str(len(successPosts)-1))
         print("Wrong: " + str(len(failPosts)-1))
+
+        for captured in failPosts:
+            print(captured)
+
         #print("Correct: " + str(len(successPosts)/(len(successPosts)+len(failPosts))))
         #print("Fail: " + str(len(failPosts)/(len(successPosts)+len(failPosts))))
         input()
-        """
-        for captured in failPosts:
-            print(captured)
-        
-        print("Correct: " + str(len(successPosts)/(len(successPosts)+len(failPosts))))
-        print("Fail: " + str(len(failPosts)/(len(successPosts)+len(failPosts))))
-
-                #redditList = open("Posts.txt", "r", encoding = 'utf-8')
-        """
-
-        """
-        #Compare Title with Body
-            
-        #Conditions
-            Capture between wwww. and .com
-            If no body compare with url
-
-        #Optimizations
-            There is a possibility that there is no url to store. Most likely found in comments by OP
-
-        postTitle = ""
-        postBody = ""
-        readBody = False 
-        successPosts = [()]
-        failPosts = [()]
-        for x in redditList:
-            i = i + 1
-            if "Title:" in x:
-                postTitle = x
-            if "Body:" in x:
-                start = x.find("://")
-                end = x.find(".com")
-                for link in range(start, end):
-                    postBody += x[link]
-                postBody = postBody.replace("://", "")
-                postBody = postBody.replace("www.", "")
-            if "Url:" in x:
-                if "Daily Chat" in postTitle:
-                    postBody = ""
-                    continue
-                if postBody == "":
-                    start = x.find("://")
-                    end = x.find(".com")
-                    for link in range(start, end):
-                        postBody += x[link]
-                    postBody = postBody.replace("://", "")
-                    postBody = postBody.replace("www.", "")
-                
-                
-                if postBody != "" and str(postBody.lower()) in str(postTitle.lower()):
-                    successPosts.append((postTitle, postBody))
-                else:
-                    failPosts.append((postTitle, postBody))
-            postBody = ""
-        """
