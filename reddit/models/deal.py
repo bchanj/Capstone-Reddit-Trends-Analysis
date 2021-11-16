a51126b0ea8c5ab86936f1b0c11d71cc9cc994b5
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2021-11-15 17:26:33 -0800

    Parse submission bodies that contain tables into deal objects

--- reddit/models/deal.py
+++ reddit/models/deal.py
@@ -6,12 +6,4 @@ class Deal:
         self.title = None
         self.discount = None
         self.merchant = None
-
-    # Copy deal into this deal if fields in this deal are null.
-    def copyWith(self, deal: Deal) -> None:
-        if self.title is None:
-            self.title = deal.title
-        if self.discount is None:
-            self.discount = deal.discount
-        if self.merchant is None:
-            self.merchant = deal.merchant
+        self.containsTable = None
