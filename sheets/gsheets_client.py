ad3542d52666582615f99e4b3adc9289321c23fe
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-01-10 11:47:19 -1000

    Intent: Outline functions with DOCSTRINGS

--- sheets/gsheets_client.py
+++ sheets/gsheets_client.py
@@ -1,4 +1,5 @@
 import gspread
+import datetime
 from typing import List
 from pathlib import Path
 from google.oauth2.service_account import Credentials
@@ -17,19 +18,45 @@ class GSClient():
         # create Google API Credentials from service_credentials found in parent directory
         creds = Credentials.from_service_account_file(
             Path(__file__).parents[0] / "service_credentials.json", scopes=self._scope)
+        
         self.client = gspread.authorize(creds)
 
-    # Returns the id of the spreadsheet that is created
-    # post-conditions: shares spreadsheets with contributors
-    def create(self, title: str) -> str:
+    def createSheet(self, parent_folder: str, title: str=datetime.datetime.now().strftime("%m-%d-%Y")) -> str:
+        """Returns a link to the spreadsheet that is created
+
+        Args:
+            parent_folder_ids (str): Link to the parent folder for the spreadsheet. Spreadsheets must have a parent folder.
+            title (str, optional): Optional title for the spreadsheet. Defaults to preformatted datetime string for the current day - datetime.datetime.now().strftime("%m-%d-%Y").
+
+        Returns:
+            str: Link to the created spreadsheet. The link also contains the key for the spreadsheet. It can be parsed.
+        """
         id: str = self.client.create(title)
         sheet: gspread.models.Spreadsheet = self.client.open_by_key(id)
-        sheet.share(emails, perm_type='user', role='owner')
+        for email in self.contributors:
+            sheet.share(emails, perm_type='user', role='owner')
         return id
 
-    def open(self, id: int) -> gspread.models.Spreadsheet:
-        sheet: gspread.models.Spreadsheet = self.client.open_by_key(id)
-        return sheet
+    def createFolder(self, title: str) -> str:
+        pass
 
-    def append(self, id: int, data: Deal) -> None:
+    def getSheet(self, parent_folder: str, title: str) -> str:
+        """Get spreadsheet link with parent folder link and spreadsheet title.
+
+        Args:
+            parent_folder (str): Link to the parent folder
+            title (str): The title of the spreadsheet.
+
+        Returns:
+            str: A link to the spreadsheet found by the title provided.
+        """
+        pass
+
+    def appendToSheet(self, sheet_link: str, data: List[Deal]) -> None:
+        """Append new data rows to a sheet.
+
+        Args:
+            sheet_link (str): A link to the Google Sheet. See -> getSheet()
+            data (List[Deal]): A list of data rows to append to the Google spreadsheet.
+        """
         sheet = self.open(id)
