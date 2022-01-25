b55a11455487bbba06a9c2072fa464ded74a6ea8
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-01-25 09:53:46 -1000

    Deal was not updated in this branch

--- sheets/models/deal.py
+++ sheets/models/deal.py
@@ -1,21 +1,111 @@
-import datetime
-from typing import List
+import re
+import praw
 
-class Deal():
-    def __init__(self, 
-        # subreddit is a required parameter
-        subreddit: str, 
-        title: str, 
-        date: str=datetime.datetime.now().strftime("%m-%d-%Y")):
-        if subreddit == None or not subreddit.strip():
-            raise Exception("Deal objects must have a subreddit")
-        self.subreddit = subreddit
+from typing import Dict
 
-        # default to datetime now if no date is provided
-        self.date = datetime.datetime.now().strftime("%m-%d-%Y") if date is None else date
-        
+class Deal:
+    def __init__(self):
+        self.title = None
+        self.discount = None
+        self.merchant = None
+        self.containsTable = None
+    def __init__(self, title: str=None, merchant: str=None, discount: str=None, price: str=None, synonyms: Dict[str, str]={}):
         self.title = title
-        
-    # get all named attributes for this object as a list of str
-    def getAttributes() -> List[str]:
-        [attr for attr in c.__dict__.keys() if attr[:1] != '_']
+        self.discount = discount
+        self.merchant = merchant
+        self.price = price
+        self.synonyms = synonyms
+
+    def isValid(self) -> bool:
+        """Returns True if deal contains:
+        1. title
+        2. discount or price
+
+        Returns:
+            bool: True if deal contains required fields else False
+
+        Unit Tests:
+        >>> d = Deal(title="ABC", discount="ABC")
+        >>> d.isValid()
+        True
+        >>> d = Deal(title="ABC", price="ABC")
+        >>> d.isValid()
+        True
+        >>> d = Deal(merchant="ABC")
+        >>> d.isValid()
+        False
+        """
+        return self.title != None and ( self.discount != None or self.price != None ) 
+
+    def setAttribute(self, synonym: str, value: str) -> None:
+        """Set class attribute with regular expression matching.
+        Uses self.synonym: Dict to match attribute names.
+        Upon successful match, will parse the value
+
+        Args:
+            synonym (str): Synonym to match with an atrribute
+            value (str): value to set the attribute with
+
+        Unit Tests:
+        >>> d = Deal(synonyms={"title": [r"^[G|g]ame$"], "discount": [r"^[S|s]ale$"],"price": [r"^[U|u][S|s][D|d]$"],})
+        >>> d.setAttribute("404NotFound", 0)
+        >>> d.title == None
+        True
+        >>> d.discount == None
+        True
+        >>> d.price == None
+        True
+        >>> d.merchant == None
+        True
+        >>> d.setAttribute("Sale", "80%")
+        >>> d.discount == "80%"
+        True
+        >>> d.setAttribute("game", "Halo Infinite")
+        >>> d.title == "Halo Infinite"
+        True
+        >>> d.setAttribute("USD", "$99")
+        >>> d.price == "$99"
+        True
+        """
+        for attribute, synonym_list in self.synonyms.items():
+            if any([ re.compile(regex).match(synonym) for regex in synonym_list ]):
+                setattr(self, attribute, value)
+                break
+
+
+class GameDeal(Deal):
+    """Deal child class representing game deals.
+    Purpose: provide a synonym dictionary to set deal attributes
+
+    Args:
+        Deal ([type]): Super class -> Do not remove
+    
+    Unit Tests:
+    >>> g = GameDeal()
+    >>> g.setAttribute("Sale", "80%")
+    >>> g.discount == "80%"
+    True
+    >>> g.setAttribute("game", "Halo Infinite")
+    >>> g.title == "Halo Infinite"
+    True
+    >>> g.setAttribute("USD", "$99")
+    >>> g.price == "$99"
+    True
+    """
+    def __init__(self, title: str=None, merchant: str=None, discount: str=None, price: str=None):
+        super().__init__(
+            title=title, 
+            merchant=merchant, 
+            discount=discount, 
+            price=price, 
+            synonyms={
+            "title": [
+                r"^[G|g]ame$"],
+            "discount": [
+                r"^[S|s]ale$"],
+            "price": [
+                r"^[U|u][S|s][D|d]$"],})
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
