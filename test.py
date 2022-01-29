989113dba56f06ebbb0e7a60677766b025af80c7
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-01-28 14:34:37 -1000

    Only test 1st party pkgs

--- test.py
+++ test.py
@@ -13,32 +13,38 @@ if __name__ == '__main__':
         ".py"
     ]
 
+    packages: List[str] = [
+        os.path.join('.', 'reddit'),
+        os.path.join('.', 'sheets')
+    ]
+
     successful = 0
     failures = 0
-    for root, dirs, files in os.walk('.'):
-        path = root.split(os.sep)
-        # skip files in root directory, we only want to test packages
-        if len(path) <= 1:
-            continue
-        for file in files:
-            if any([file.endswith(ext) for ext in target_extensions]):
-                print(Fore.WHITE + f"""
+    for package in packages:
+        for root, dirs, files in os.walk(package):
+            path = root.split(os.sep)
+            for file in files:
+                if any([file.endswith(ext) for ext in target_extensions]):
+                    print(Fore.WHITE + f"""
 =========================================                
 RUN: Running tests for: {os.path.join(root, file)}""")
-                file_path = os.path.join(root, file)
-                try:
-                    subprocess.run(['python3', file_path], check = True)
-                except subprocess.CalledProcessError as e:
-                    failures += 1
-                    print("Tests Failed")
-                    print(Fore.RED + f"""FAILURE: {file}, Path: {file_path}.
+                    file_path = os.path.join(root, file)
+                    try:
+                        subprocess.run(['python3', file_path], check = True)
+                    except subprocess.CalledProcessError as e:
+                        failures += 1
+                        print("Tests Failed")
+                        print(Fore.RED + f"""FAILURE: {file}, Path: {file_path}.
 Here was the error {e}""")
-                else:
-                    successful += 1
-                    print(Fore.GREEN + f"""SUCCESS: Tests for {file} succeeded.""")
+                    else:
+                        successful += 1
+                        print(Fore.GREEN + f"""SUCCESS: Tests for {file} succeeded.""")
     print()
     print()
     print(Fore.WHITE + "Test Summary")
     print(Fore.GREEN + f"""Successful Tests: {successful}""")
     print(Fore.RED + f"""Failed Tests: {failures}""")
     print(Fore.WHITE)
+
+    if (failures > 0):
+        raise Exception(f"Test Pass Failed: {failures} tests failed")
