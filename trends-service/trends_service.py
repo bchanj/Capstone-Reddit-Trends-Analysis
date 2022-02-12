0bf70dcaa76ac9a8ee48058133e1409bf0ba3b0c
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-02-12 14:02:01 -0800

    Add Kriston's code to trends_service to serve html

--- trends-service/trends_service.py
+++ trends-service/trends_service.py
@@ -22,3 +22,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+def createTable(self, sheetDict: Dict[str, List[str]]):
+    """Creates an HTML table template
+    TODO at the moment this opens a file and dumps the table code to be copy-paste; will likely need pyppeteer to dump table into email client
+
+    Args:
+        sheetDict (dict): A dictionary with the name of the table column as the key, and a list for the table cells
+
+    Unit Tests: TODO
+        # Test that a table is created with the correct information inserted
+        >>> dict: Dict[str, List[str]] = { "x": ["1", "2"], "y": ["1", "3"] }
+        >>> client: GSClient = GSClient()
+        >>> client.createTable(dict)
+        "<tr><th>x</th><th>y</th></tr><tr><td>1</td><td>1</td></tr><tr><td>2</td><td>3</td></tr>"
+    """
+    data = ""
+    for k, v in sheetDict.items():
+        data += "<tr>"
+        data += "<th>" + k + "</th>"
+        if isinstance(v, list):
+            for value in v:
+                data += "<td>" + value + "</td>"
+        data += "</tr>"
+    table = open("table_template.html").read().format(table_contents=data)
+    return table
