a51b6edcf588356556a2784a303fed4eac1b4333
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-02-12 15:39:46 -0800

    Update sys path

--- sheets/SheetsAzureFunctions.py
+++ sheets/SheetsAzureFunctions.py
@@ -1,6 +1,8 @@
-import os
 import sys
-sys.path.append(os.path.dirname(__file__))
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'cosmos', 'models'))
+
 from models import gsheets_client
 from cgi import test
 from importlib.resources import path
