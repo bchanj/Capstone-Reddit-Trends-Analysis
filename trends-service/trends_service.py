12f17939a4e697ac974f42bd574985e5ca218d62
Stephen (Alex) Wallen <44445638+a-wallen@users.noreply.github.com>
2022-02-12 15:40:03 -0800

    Add container functionality to output html summaries

--- trends-service/trends_service.py
+++ trends-service/trends_service.py
@@ -1,49 +1,60 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'models'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'cosmos', 'models'))
+
 import logging
 
 import azure.functions as func
-
+from cosmos_db_wrapper import CosmosClientWrapper
+from typing import Dict, List
+from collections import defaultdict
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    def createTable(sheetDict: Dict[str, List[str]]):
+        """Creates an HTML table template
+        TODO at the moment this opens a file and dumps the table code to be copy-paste; will likely need pyppeteer to dump table into email client
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+        Args:
+            sheetDict (dict): A dictionary with the name of the table column as the key, and a list for the table cells
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+        Unit Tests: TODO
+            # Test that a table is created with the correct information inserted
+            >>> dict: Dict[str, List[str]] = { "x": ["1", "2"], "y": ["1", "3"] }
+            >>> client: GSClient = GSClient()
+            >>> client.createTable(dict)
+            "<tr><th>x</th><th>y</th></tr><tr><td>1</td><td>1</td></tr><tr><td>2</td><td>3</td></tr>"
+        """
+        data = ""
+        for k, v in sheetDict.items():
+            data += "<tr>"
+            data += "<th>" + k + "</th>"
+            if isinstance(v, list):
+                for value in v:
+                    data += "<td>" + value + "</td>"
+            data += "</tr>"
+        return data
+
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
+        cosmosDbWrapper = CosmosClientWrapper()
+        extract = cosmosDbWrapper.readEntries()
+        res = defaultdict(list)
+        for sub in extract:
+            for key in sub:
+                res[key].append(sub[key])
+        data = createTable(res)
+        preamble = open("table_template.html").read()
+        table = open("table_template.html").read().format(table_contents=data)
+        print(preamble + table)
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            status_code=200,
+            headers={'content-type':'text/html'}, 
+            body=(preamble+table))
+    except (e):
+        message=f"Internal Server Error: {e}"
+        logging.info(message)
+        return func.HttpResponse(
+             status_code=500,
+             
         )
-
-def createTable(self, sheetDict: Dict[str, List[str]]):
-    """Creates an HTML table template
-    TODO at the moment this opens a file and dumps the table code to be copy-paste; will likely need pyppeteer to dump table into email client
-
-    Args:
-        sheetDict (dict): A dictionary with the name of the table column as the key, and a list for the table cells
-
-    Unit Tests: TODO
-        # Test that a table is created with the correct information inserted
-        >>> dict: Dict[str, List[str]] = { "x": ["1", "2"], "y": ["1", "3"] }
-        >>> client: GSClient = GSClient()
-        >>> client.createTable(dict)
-        "<tr><th>x</th><th>y</th></tr><tr><td>1</td><td>1</td></tr><tr><td>2</td><td>3</td></tr>"
-    """
-    data = ""
-    for k, v in sheetDict.items():
-        data += "<tr>"
-        data += "<th>" + k + "</th>"
-        if isinstance(v, list):
-            for value in v:
-                data += "<td>" + value + "</td>"
-        data += "</tr>"
-    table = open("table_template.html").read().format(table_contents=data)
-    return table
