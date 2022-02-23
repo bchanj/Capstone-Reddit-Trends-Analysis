8c6b57608dac59ceff752590f7169748fe989801
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-23 14:51:15 -0800

    Update installer to read from requirements.txt

--- installer.py
+++ installer.py
@@ -2,22 +2,6 @@
 from pip._internal import main as pipmain
 import platform
 
-pkg_list = [
-    "praw",
-    "google-auth-httplib2",
-    "google-api-python-client",
-    "google-auth-oauthlib",
-    "gspread",
-    "requests",
-    "argparse",
-    "typing",
-    "pathlib",
-    "python-dotenv",
-    "colorama",
-    "bs4",
-    "azure-cosmos",
-]
-
 def import_or_install(package):
     try:
         __import__(package)
@@ -25,20 +9,17 @@ def import_or_install(package):
         pipmain(['install', package])   
 
 def getDependencies():
-    # Check the current Python Version
-    # PRAW requires Python > 3.6
     assert (int(platform.python_version_tuple()[0]) >= 3 and int(platform.python_version_tuple()[1]) >= 6), """
         Python version is not greater than 3.6, some packages in this repository require python 3.6 >.
     """
-
-    # pip3 should be automatically installed with all python versions greater than 3.4
-    # see: https://docs.python.org/3/installing/index.html
-    for package in pkg_list:
-        import_or_install(package)
+    with open("requirements.txt") as requirements:
+        for package in [line.rstrip() for line in requirements if line.rstrip() and not line.startswith('#')]:
+            import_or_install(package)
 
 def cleanDependencies():
-    for package in pkg_list:
-        pipmain(['uninstall', package])
+    with open("requirements.txt") as requirements:
+        for package in [line.rstrip() for line in requirements if line.rstrip() and not line.startswith('#')]:
+            pipmain(['uninstall', package])
 
 if __name__ == "__main__":
     getDependencies()
