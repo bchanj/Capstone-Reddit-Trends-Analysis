2b201c9f3226cbec66c60843d85573b4523505f9
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2022-02-23 14:51:03 -0800

    Update cosmos to read from encrypted credentials

--- cosmos/models/cosmos_db_wrapper.py
+++ cosmos/models/cosmos_db_wrapper.py
@@ -7,22 +7,29 @@ sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.
 import os
 import base64
 import json
-import dotenv
+import dotenv 
 from azure.cosmos import CosmosClient
-from bundle import Bundle
+from deal import Deal
 from query_filter import QueryFilter
 from typing import List
 
 class CosmosClientWrapper():
     def __init__(self):
-        self._internalClient = CosmosClient(
-            "https://cs2215newtest.documents.azure.com:443/",
-            "o2rmrQEOk9rAqBCVmh4nxpzwRfJQ2y5Ayr2g8q7GTEcRhPuBudjgIrBRgIjrvKOimrO0066nBmb9oSXsrYIfew==",
+        self._internalClient = self._clientCreateInstance()
+        self.database_name = 'Deals'
+        self._database = self._internalClient.get_database_client(self.database_name)
+        self.container_name = 'gameDeals'
+        self._container = self._database.get_container_client(self.container_name)
+
+    def _clientCreateInstance(self):
+        dotenv.load_dotenv()
+        encrypted_creds: str = os.environ["COSMOS_CREDS"]
+        decoded: bytes = base64.b64decode(encrypted_creds)
+        json_dict = json.loads(decoded)
+        return CosmosClient(
+            json_dict["uri"],
+            json_dict["PRIMARY_KEY"]
         )
-        database_name = 'ToDoList'
-        self._database = self._internalClient.get_database_client(database_name)
-        container_name = 'Items'
-        self._container = self._database.get_container_client(container_name)
 
     def _createContainer(self) -> None:
         try:
@@ -32,27 +39,28 @@ class CosmosClientWrapper():
         except exceptions.CosmosHttpResponseError:
             raise
 
-    def createEntries(self, bundles: List[Bundle]) -> None:
-        for bundle in bundles:
-            # print(bundle.__dict__)
-            self._container.upsert_item(bundle.__dict__)
+    def createEntries(self, deals: List[Deal]) -> None:
+        for deal in deals:
+            # print(deal.__dict__)
+            self._container.upsert_item(deal.__dict__)
 
-    def readEntries(self, filters: List[QueryFilter]=None, start: int=0, limit:int=10):
+    def readEntries(self, filters: List[QueryFilter]=None, start: int=0, limit:int=10) -> List[Deal]:
         clauses = ""
         whereClause = ""
         if filters is not None:
             clauses = " AND ".join([f'CONTAINS(c.{f.key}, "{f.value}")' for f in filters])
             whereClause = f"WHERE {clauses}"
-        print(whereClause)
         result = []
-        for item in self._container.query_items(
-        query=f'''
+        q = f'''
             SELECT * 
-            FROM c IN Items.items 
+            FROM c
             {whereClause}
-            OFFSET {start} LIMIT {limit}''',
+            OFFSET {start} LIMIT {limit}'''
+        for item in self._container.query_items(
+        query=q,
         enable_cross_partition_query=True):
-            result.append(item)
+            excluding_private = {x: item[x] for x in item if not x.startswith("_")}
+            result.append(Deal(**excluding_private))
         return result
 
 
